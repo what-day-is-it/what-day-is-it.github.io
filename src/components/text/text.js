524a8674dc15490a8942ae523071078e0c066b54
Nayyir Jutha <njutha@apigarage.com>
2020-04-04 22:04:10 -0400

    Updates

--- src/components/text/text.js
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react';
-import Heading from './components/heading';
-import Body from './components/body';
-
-export default class Text extends Component {
-  static Heading = Heading;
-
-  static Body = Body;
-}
--- src/router/routes.config.js
+++
@@ -1,8 +0,0 @@
-import Landing from 'views/landing/landing';
-
-export default [
-  {
-    path: "/",
-    component: Landing
-  }
-];
